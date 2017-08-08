2a1d51df4410eca3f30b36dfb5f4e959bd1281f9
GLProgrammer <jirka2002@gmail.com>
2017-08-08 13:49:44 -0700

    BETTER SENDING
    BETTER SENDING

--- DvojtejRobot/src/Robot97/Robot97_Main.java
+++ DvojtejRobot/src/Robot97/Robot97_Main.java
@@ -1,10 +1,41 @@
 package Robot97;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import lejos.nxt.Sound;
+import lejos.nxt.comm.NXTConnection;
+import lejos.nxt.comm.RS485;
+
 public class Robot97_Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String name = "BOT 6";
+		System.out.println("Connecting...");
+		NXTConnection con = RS485.connect("BOT 6", NXTConnection.PACKET);
+		if (con == null) {
+			System.out.println("Connect fail");
+			Sound.twoBeeps();
+			Thread.sleep(5000);
+			System.exit(1);
+		}
+			System.out.println("Connected!!");
+			DataInputStream dis = con.openDataInputStream();
+			DataOutputStream dos = con.openDataOutputStream();
+			while (true){
+			try{		//POSILANI
+				dos.writeInt(0);
 
+				dos.flush();
+				Thread.sleep(500);
+				dos.writeInt(100);
+
+				dos.flush();
+			}
+			catch (IOException ioe) {
+				System.out.println("Write Exception");
+			}
 	}
 
 }
