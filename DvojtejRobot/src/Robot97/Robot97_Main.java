0a3e9e3265971fb6e36cd84318402332f4f30ea2
GLProgrammer <jirka2002@gmail.com>
2017-08-08 14:21:52 -0700

    Added Remote controll
    Added Remote controll

--- DvojtejRobot/src/Robot97/Robot97_Main.java
+++ DvojtejRobot/src/Robot97/Robot97_Main.java
@@ -4,11 +4,43 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import lejos.nxt.Button;
 import lejos.nxt.Sound;
 import lejos.nxt.comm.NXTConnection;
 import lejos.nxt.comm.RS485;
 
 public class Robot97_Main {
+
+	private static NXTConnection con;
+	private static DataInputStream dis;
+	private static DataOutputStream dos;
+
+	public static void Posli(int Stav, int Parametr) throws InterruptedException{
+		try{		//POSILANI
+			dos.writeInt(Stav);
+			dos.flush();
+			Thread.sleep(100);
+			dos.writeInt(Parametr);
+			dos.flush();
+			Thread.sleep(100);
+		}
+		catch (IOException ioe) {
+			System.out.println("Write Exception");
+		}
+	}
+
+	public static void Posli(int Stav) throws InterruptedException{
+
+		try{		//POSILANI
+			dos.writeInt(Stav);
+			dos.flush();
+			Thread.sleep(100);
+		}
+		catch (IOException ioe) {
+			System.out.println("Write Exception");
+		}
+	}
+
 
 	public static void main(String[] args) throws InterruptedException {
 		String name = "BOT 6";
@@ -21,9 +53,30 @@ public class Robot97_Main {
 			System.exit(1);
 		}
 			System.out.println("Connected!!");
-			DataInputStream dis = con.openDataInputStream();
-			DataOutputStream dos = con.openDataOutputStream();
+			dis = con.openDataInputStream();
+			dos = con.openDataOutputStream();
 			while (true){
+
+			switch (Button.readButtons()) {
+			case Button.ID_ENTER:		//Jed 			0
+					Posli(0, 100);
+				break;
+
+			case Button.ID_LEFT:		//Zatoc +		1
+					Posli(1, 90);
+				break;
+
+			case Button.ID_RIGHT:		//Zatoc -		1
+					Posli(1, -90);
+				break;
+
+			case Button.ID_ESCAPE:		//Zastav		2
+					Posli(2);
+				break;
+			default:
+				break;
+			}
+
 			try{		//POSILANI
 				dos.writeInt(0);
 
