abcf66e2f09c1f77a29c7c16808da5adb6ed61a4
GLProgrammer <jirka2002@gmail.com>
2017-08-08 02:31:32 -0700

    Added Two projects
    Added Two projects

---
+++ Test Recieve/src/Test_Recieve.java
@@ -0,0 +1,103 @@
+import lejos.util.TextMenu;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.nxt.comm.*;
+import java.io.*;
+
+public class Test_Recieve {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		// System.out.println(RS485.isAddress("00165318BD7A"));	//00165318D9FD - BOT 6	//00165318BD7A - BOT 97
+		//String[] connectionStrings = new String[]{"Bluetooth", "USB", "RS485"};
+		//TextMenu connectionMenu = new TextMenu(connectionStrings, 0, "Connection");
+		//String[] modeStrings = new String[] {"Packet", "Raw"};
+		//TextMenu modeMenu = new TextMenu(modeStrings, 0, "Mode");
+		//NXTCommConnector[] connectors = {Bluetooth.getConnector(), USB.getConnector(), RS485.getConnector()};
+		//int[] modes = {NXTConnection.PACKET, NXTConnection.RAW};
+
+		//int connectionType = connectionMenu.select();
+		//LCD.clear();
+		//int mode = modeMenu.select();
+		while (true){
+			/*LCD.clear();
+			LCD.drawString("Type:       " + connectionStrings[connectionType], 0, 0);
+			LCD.drawString("Mode: " + modeStrings[mode], 0, 1);*/
+			System.out.println("Waiting...");
+
+			NXTConnection con = RS485.waitForConnection(0, NXTConnection.PACKET);
+
+			System.out.println("Connected...");
+
+			DataInputStream dis = con.openDataInputStream();
+			DataOutputStream dos = con.openDataOutputStream();
+
+			while (true){
+				char n = ' ';
+				try {
+					n = dis.readChar();
+				} catch (EOFException e) {
+					break;
+				}
+				if (n != ' ') {
+					System.out.println("Read: " + n);
+					/*switch (n.charAt(1)) {
+					case '0':	//FWD
+
+							Motor.C.backward();
+							Motor.B.backward();
+							Thread.sleep(1000);
+							Motor.C.stop();
+							Motor.B.stop();
+						break;
+
+					case '1':	//BCKWD
+
+						Motor.C.forward();
+						Motor.B.forward();
+						Thread.sleep(1000);
+						Motor.C.stop();
+						Motor.B.stop();
+						break;
+
+					case '2':	//Left
+
+						Motor.C.backward();
+						Motor.B.forward();
+						Thread.sleep(1000);
+						Motor.C.stop();
+						Motor.B.stop();
+					break;
+
+					case '3':	//Right
+
+						Motor.C.forward();
+						Motor.B.backward();
+						Thread.sleep(1000);
+						Motor.C.stop();
+						Motor.B.stop();
+					break;
+
+					case '4':	//UP
+
+						Motor.A.forward();
+						Thread.sleep(250);
+						Motor.A.stop();
+					break;
+
+					default:	//Read ERROR
+
+						System.out.println("READ ERROR");
+						break;
+					}*/
+				}
+
+
+			}
+			System.out.println("Closing...");
+			dis.close();
+			dos.close();
+			con.close();
+		}
+	}
+
+}
---
+++ Test Send/src/Test_Send.java
@@ -0,0 +1,90 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import lejos.nxt.*;
+import lejos.nxt.comm.*;
+import lejos.util.Delay;
+import lejos.util.TextMenu;
+
+
+public class Test_Send{
+	public static void main(String[] args) throws InterruptedException {
+		String name = "BOT 6";
+		//String[] connectionStrings = {"Bluetooth", "RS485"};
+		//TextMenu connectionMenu = new TextMenu(connectionStrings, 0, "Connection");
+			//String[] modeStrings = {"Packet", "Raw"};
+		//TextMenu modeMenu = new TextMenu(modeStrings, 0, "Mode");
+			//NXTCommConnector[] connectors = {Bluetooth.getConnector(),RS485.getConnector()};
+			//int[] modes = {NXTConnection.PACKET, NXTConnection.RAW};
+
+		//int connectionType = connectionMenu.select();
+		//LCD.clear();
+		//int mode = modeMenu.select();
+
+		/*LCD.clear();
+		LCD.drawString("Name: " + name, 0, 0);
+		LCD.drawString("Type: " + connectionStrings[connectionType], 0, 1);
+		LCD.drawString("Mode: " + modeStrings[mode], 0, 2);
+		LCD.drawString("Connecting...", 0, 3);*/
+		System.out.println("Connecting...");
+		NXTConnection con = RS485.connect("BOT 6", NXTConnection.PACKET);
+
+		if (con == null) {
+			System.out.println("Connect fail");
+			Sound.twoBeeps();
+			Thread.sleep(5000);
+			System.exit(1);
+		}
+
+		System.out.println("Connected!!");
+		DataInputStream dis = con.openDataInputStream();
+		DataOutputStream dos = con.openDataOutputStream();
+
+			try {
+				/*dos.writeChars("Test");		//POSILANI
+				dos.flush();
+				dos.writeChars("Super");
+				dos.flush();
+				dos.writeChars("Awesome");
+				dos.flush();*/
+				dos.writeInt(1);
+
+				dos.flush();
+				Thread.sleep(5000);
+				/*dos.writeChars("1");
+				dos.flush();
+				Thread.sleep(5000);*/
+				/*dos.writeChars("2");
+				dos.flush();
+				Thread.sleep(5000);*/
+				/*dos.writeChars("3");
+				dos.flush();
+				Thread.sleep(5000);*/
+				/*dos.writeChars("4");
+				dos.flush();
+				Thread.sleep(5000);*/
+			} catch (IOException ioe) {
+				System.out.println("Write Exception");
+			}
+
+			/*try {
+				LCD.drawString("Read: ", 0, 7);
+				LCD.drawInt(dis.readInt(), 8, 6, 7);
+			} catch (IOException ioe) {
+				LCD.drawString("Read Exception", 0, 5);
+			}*/
+
+		try {
+			System.out.println("Closing...");
+			dis.close();
+			dos.close();
+			con.close();
+		} catch (Exception e) {
+			System.out.println("Close Exception");
+		}
+		System.out.println("Finished!!!");
+		Thread.sleep(20000);
+	}
+
+}
